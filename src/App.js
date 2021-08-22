4ce61361afd6dc723f06226dfb57de59edb6d547
DanujaV <danujagreru@gmail.com>
2021-08-23 01:46:03 +0530

    build the nav bar complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import  Row  from './Row'
 import './App.css';
 import requests from './requests';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
     return (
     <div className="app">
       {/* Nav */}
+      <Nav/>
 
       {/* Banner */}
       <Banner />
